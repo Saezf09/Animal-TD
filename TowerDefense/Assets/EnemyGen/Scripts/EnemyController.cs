ee5731d730d43d9aee04e50a0bb3dee610c90162
Owen <59458276+OClarke04@users.noreply.github.com>
2026-03-15 17:33:01 +0000

    Dog Test Tracking

---
+++ TowerDefense/Assets/EnemyGen/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject dest;
+    public Animator animator;
+    private bool isMoving;
+    private float speed;
+    float stoppingDistance;
+    
+    void Start()
+    {
+        agent.SetDestination(dest.transform.position);
+        stoppingDistance = agent.stoppingDistance;
+
+    }
+    private void Update()
+    {
+        float dist = agent.remainingDistance;
+        speed = agent.velocity.magnitude / agent.speed;
+
+        if (agent.velocity != Vector3.zero)
+        {
+            animator.SetFloat("Speed", speed);
+            animator.SetBool("isAttacking", false);
+        }
+        else if (agent.velocity == Vector3.zero)
+        {
+            animator.SetFloat("Speed", 0);
+           
+        }
+        
+        if (dist != Mathf.Infinity && agent.remainingDistance <= stoppingDistance)
+        {
+            animator.SetBool("isAttacking", true);
+        }
+    }
+
+}
